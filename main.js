aa0f9e87e02d362cd68deb284d3c775abdfbf462
Jeongin Park <jjeonginpark@gmail.com>
2020-01-07 17:16:02 +0900

    index 페이지구현

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathName = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     console.log(url.parse(_url, true));
-    console.log(pathName);
-    
-    if(pathName !== '/'){
-        fs.readFile(`data/${queryData.id}`, (err, description) => {
+
+    if(pathName === '/'){
+        if(queryData.id === undefined){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
             var template = `
             <!doctype html>
             <html>
@@ -34,7 +34,32 @@ var app = http.createServer(function(request,response){
             `;
             response.writeHead(200);
             response.end(template);
-        });
+        }else{
+            fs.readFile(`data/${queryData.id}`, (err, description) => {
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     }else{
         response.writeHead(404);
         response.end('Not Found');
