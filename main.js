e3f84a2250ac5bc0b256f135eef4ab76646bf6d8
Jeongin Park <jjeonginpark@gmail.com>
2020-01-08 16:20:01 +0900

    App제작 글목록 출력하기

--- main.js
+++ main.js
@@ -7,36 +7,19 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathName = url.parse(_url, true).pathname;
 
-    console.log(url.parse(_url, true));
-
     if(pathName === '/'){
         if(queryData.id === undefined){
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-        }else{
-            fs.readFile(`data/${queryData.id}`, (err, description) => {
-                var title = queryData.id;
+            fs.readdir('./data', (err, filelist) => {
+                console.log(filelist);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                filelist.forEach( file => {
+                    list += `<li><a href="/?id=${file}">${file}</a></li>`;
+                });
+                list = list+'</ul>';
+                
+                console.log(list);
                 var template = `
                 <!doctype html>
                 <html>
@@ -46,11 +29,7 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                     <h1><a href="/">WEB</a></h1>
-                    <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
+                    ${list}
                     <h2>${title}</h2>
                     <p>${description}</p>
                 </body>
@@ -58,6 +37,39 @@ var app = http.createServer(function(request,response){
                 `;
                 response.writeHead(200);
                 response.end(template);
+            });
+            
+        }else{
+            fs.readdir('./data', (err, filelist) => {
+                console.log(filelist);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                filelist.forEach( file => {
+                    list += `<li><a href="/?id=${file}">${file}</a></li>`;
+                });
+                list = list+'</ul>';
+
+                fs.readFile(`data/${queryData.id}`, (err, description) => {
+                    var title = queryData.id;
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}node
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
         }
     }else{
