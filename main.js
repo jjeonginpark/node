57b9e18524aa8e66e09b7986e9f37689a225630f
Jeongin Park <jjeonginpark@gmail.com>
2020-01-13 11:02:41 +0900

    App 제작 - 모듈의 활용

--- main.js
+++ main.js
@@ -3,34 +3,8 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-//Code Refactoring
-var template = {
-    HTML : function (title, list, body, control) {
-        return `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB2</a></h1>
-            ${list}
-            ${control}
-            ${body}
-        </body>
-        </html>
-        `;
-    },
-    list : function (filelist) {
-        var list = '<ul>';
-        filelist.forEach( file => {
-            list += `<li><a href="/?id=${file}">${file}</a></li>`;
-        });
-        list = list+'</ul>';
-        return list;
-    }
-}
+var template = require('./lib/template.js');
+
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
